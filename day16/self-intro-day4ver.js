d2f9fed2f510bab24197ac357605043317fee706
Feihong Wu <42343728+MartinBug@users.noreply.github.com>
2018-10-22 21:47:56 +0800

    completed!
    lalalalala

---
+++ day16/self-intro-day4ver.js
@@ -0,0 +1,6 @@
+// console.log(document.getElementById("content-wrapper").innerHTML);
+
+document.getElementById("hello-button").addEventListener("click", function () {
+    // document.getElementById(hello-message).innerText = "Hello!";
+    alert("I am in.")
+})
